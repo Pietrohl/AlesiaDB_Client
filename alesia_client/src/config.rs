b2d84725fd469c1457f2ca3bf3a03f2476ab5b9d
Pietro Henrique <pietro_henrique4@hotmail.com>
2024-04-12 17:15:13 -0300

    Refactor of Config struct  function

--- alesia_client/src/config.rs
+++ alesia_client/src/config.rs
@@ -1,85 +1,64 @@
-use url::Url;
+use std::net::{SocketAddr, ToSocketAddrs};
 
-use crate::{client::AlesiaClient, errors::Error};
+use crate::{client::AlesiaClient, errors};
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Config {
-    pub(crate) path: String,
+    pub(crate) path: SocketAddr,
 }
 
 impl Config {
-    pub fn from_str(url: &str) -> Result<Self, Error> {
-        if !is_url(url) && !is_ip_addr(url) {
-            return Err(Error::ConfigError("Invalid URL".into()));
-        }
+    pub fn from_str(url: &str) -> Result<Self, errors::Error> {
+        let addrs: String = url.to_string();
 
-        Ok(Self {
-            path: url.to_string(),
-        })
+        let mut addrs_iter = (&addrs[..])
+            .to_socket_addrs()
+            .map_err(|_| errors::Error::ConfigError("Invalid URL".into()))?;
+
+        let path = addrs_iter.next().unwrap();
+        Ok(Self { path })
     }
 
-    pub async fn connect(self) -> Result<AlesiaClient, Error> {
+    pub async fn connect(self) -> Result<AlesiaClient, errors::Error> {
         AlesiaClient::create(self).await
     }
-}
-
-fn is_url(url: &str) -> bool {
-    Url::parse(url).is_ok()
-}
-
-fn is_ip_addr(url: &str) -> bool {
-    let mut url_parts = url.rsplitn(2, ':');
-    let tcp_port = url_parts.next();
-    let ip = url_parts.next();
-
-    if None == tcp_port || None == ip {
-        return false;
-    }
-
-    let ip_parts: Vec<&str> = ip.unwrap().split('.').collect();
-
-    if ip_parts.len() != 4 {
-        return false;
-    }
-
-    for part in ip_parts {
-        if let Err(_) = part.parse::<u8>() {
-            return false;
-        }
-    }
-
-    if let Err(_) = tcp_port.unwrap().parse::<u16>() {
-        return false;
-    }
-
-    true
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-     #[test]
-     fn test_config_from_str_valid_url() {
-         let url = "https://example.com";
-         let config = Config::from_str(url).unwrap();
-         assert_eq!(config.path, url);
-     }
-    
-      #[test]
-      fn test_valid_local_url() {
-          let url = "http://localhost:8000";
-          let config = Config::from_str(url).unwrap();
-          assert_eq!(config.path, url);
-          let url = "127.0.0.1:8080";
-          let config = Config::from_str(url).unwrap();
-          assert_eq!(config.path, url);
-      }
-    
-      #[test]
-      fn test_config_from_str_invalid_url() {
-          let url = "invalid_url";
-          let result = Config::from_str(url);
-          assert!(result.is_err());
-      }
+    #[test]
+    fn test_config_from_str_valid_url() {
+        let url = "www.example.com:80";
+        let config = Config::from_str(url);
+        assert!(config.is_ok());
+    }
+
+    #[test]
+    fn test_valid_local_url() {
+        let url = "localhost:8000";
+        let config = Config::from_str(url).unwrap();
+        assert_eq!(config.path.to_string(), "[::1]:8000");
+    }
+    #[test]
+    fn test_valid_local_socket_addrs() {
+        let url = "127.0.0.1:8080";
+        let config = Config::from_str(url).unwrap();
+        assert_eq!(config.path.to_string(), url);
+    }
+
+    #[test]
+    fn test_valid_local_zero_socket_addrs() {
+        let url = "0.0.0.0:8080";
+        let config = Config::from_str(url).unwrap();
+        assert_eq!(config.path.to_string(), url);
+    }
+
+    #[test]
+    fn test_config_from_str_invalid_url() {
+        let url = "invalid_url";
+        let result = Config::from_str(url);
+        assert!(result.is_err());
+    }
 }
